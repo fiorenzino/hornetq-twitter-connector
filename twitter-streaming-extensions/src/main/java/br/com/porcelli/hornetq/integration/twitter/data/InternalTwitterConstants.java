6a16b2945f45402a57b61483121c03e2b97efc0d
Alexandre Porcelli <alexandre.porcelli@gmail.com>
2010-12-21 19:26:33 -0200

    added lic

--- twitter-streaming-extensions/src/main/java/br/com/porcelli/hornetq/integration/twitter/data/InternalTwitterConstants.java
+++ twitter-streaming-extensions/src/main/java/br/com/porcelli/hornetq/integration/twitter/data/InternalTwitterConstants.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2010 Alexandre Porcelli <alexandre.porcelli@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package br.com.porcelli.hornetq.integration.twitter.data;
 
 import java.util.HashSet;
@@ -7,11 +22,6 @@ import org.hornetq.api.core.SimpleString;
 
 import br.com.porcelli.hornetq.integration.twitter.TwitterConstants;
 
-/**
- * A TwitterConstants
- * 
- * @author <a href="tm.igarashi@gmail.com">Tomohisa Igarashi</a>
- */
 public class InternalTwitterConstants implements TwitterConstants {
 
     public static final SimpleString LAST_TWEET_ID_VALUE           = new SimpleString("last_tweet");
